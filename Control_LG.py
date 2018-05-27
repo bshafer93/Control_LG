fa7c12a83955409ad30f9454259a3b4334396da0
bshafer93 <bshafer93@gmail.com>
2018-05-27 16:02:15 -0700

    1

--- Control_LG.py
+++ Control_LG.py
@@ -24,7 +24,7 @@ class LGC7:
 
     def Receive_Data(self,ser):
         data = ser.read(4096)
-        print ("received message:", data.decode('utf-8'))
+        print ("received message:", data)
         ser.close()
 
 
