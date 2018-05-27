07ad53fa5711da95a201604f3f428ea3d9187a5d
bshafer93 <bshafer93@gmail.com>
2018-05-27 15:24:49 -0700

    ini

---
+++ Control_LG.py
@@ -0,0 +1,32 @@
+import serial 
+
+class LGC7:
+
+    def __init__(self,BD_PORT,BD_BAUDRATE):
+        self.BD_PORT = BD_PORT
+        self.BD_BAUDRATE = BD_BAUDRATE
+
+    def ON(self):
+            self.Send_Cmd('ka 00 01')
+            #Discrete on
+    def OFF(self):
+            self.Send_Cmd('ka 00 00')
+            #Discrete off
+            
+    ### Query COMMANDS    
+    def Send_Cmd(self,cmd):
+        ser = serial.Serial(self.BD_PORT,timeout = 2)
+       
+        ser.baudrate = self.BD_BAUDRATE
+        full_cmd = bytes(cmd + "\r",'utf-8')
+        ser.write(full_cmd)
+        self.Receive_Data(ser)
+
+    def Receive_Data(self,ser):
+        data = ser.read(4096)
+        print ("received message:", data.decode('unicode_escape'))
+        ser.close()
+
+
+
+
---
+++ LG_Test.py
@@ -0,0 +1,9 @@
+from Control_LG import LGC7
+
+import time
+
+MyLG = Oppo203('/dev/ttyUSB1',9600)
+
+
+
+MyLG.PowerStatus()
